eef3e837b7d77aa6998df26f8c7d5f862a0c861c
Daniel Lim-Apo <nomail>
2024-09-04 22:36:25 -0300

    Example 001 CRUD

---
+++ 001-CRUD/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3306/test";
+        String username = "root";
+        String password = "SpassWord!2";
+        System.out.println("Connecting...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+
+    }
+}
